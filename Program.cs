e32c7145649167d208bfc2eb69f386d878d31599
DESKTOP-DN60QRR\ПК <st8645855@stud.duikt.edu.ua>
2024-10-02 12:45:06 +0300

    Upgraded Program file (now can choose exercises)

--- Program.cs
+++ Program.cs
@@ -7,14 +7,29 @@ namespace Pz1
 		public const int N = 10 + 4;
 		static void Main(string[] args)
 		{
-			Ex1 ex1 = new Ex1();
-			ex1.Exercise(N);
-			Ex2 ex2 = new Ex2();
-			ex2.Exercise();
-			Ex3 ex3 = new Ex3();
-			ex3.Exercise(N);
-			Ex4 ex4 = new Ex4();
-			ex4.Exercise(N);
+
+			Console.WriteLine("Enter exercise number(1-4): ");
+			int menu = int.Parse(Console.ReadLine());
+
+			switch (menu)
+			{
+				case 1:
+					Ex1 ex1 = new Ex1();
+					ex1.Exercise(N);
+					break;
+				case 2:
+					Ex2 ex2 = new Ex2();
+					ex2.Exercise();
+					break;
+				case 3:
+					Ex3 ex3 = new Ex3();
+					ex3.Exercise(N);
+					break;
+				case 4:
+					Ex4 ex4 = new Ex4();
+					ex4.Exercise(N);
+					break;
+			}		
 		}
 	}
 }
