e88b3242566f2238a969aaf5f4a5515b34ce8267
DESKTOP-DN60QRR\ПК <st8645855@stud.duikt.edu.ua>
2024-10-02 12:24:44 +0300

    Add ex1

---
+++ Ex1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Ex1
+    {
+        private bool Compare(int N, int Number)
+        {
+            if (Number >= 1 && Number <= N)
+                return true;
+            else
+                return false;
+        }
+        public void Exercise(int N)
+        {
+            int[] Numbers = {6, 90, 12};
+
+            foreach (int i in Numbers)
+            {
+                if (Compare(N, i))
+                    Console.WriteLine($"{i} is in diapazone");
+                else
+                    Console.WriteLine($"{i} is out of diapazone");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ namespace Pz1
 {
 	internal class Program
 	{
-		public const int N = 10 + 1;
+		public const int N = 10 + 4;
 		static void Main(string[] args)
 		{
+			Ex1 ex1 = new Ex1();
+			ex1.Exercise(N);
 			Ex2 ex2 = new Ex2();
-			//ex2.Exercise();
+			ex2.Exercise();
 			Ex3 ex3 = new Ex3();
-			//ex3.Exercise(N);
+			ex3.Exercise(N);
 			Ex4 ex4 = new Ex4();
 			ex4.Exercise(N);
 		}
